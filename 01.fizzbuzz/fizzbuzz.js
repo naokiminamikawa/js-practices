0645fe775eed6133c31eefe103332baa25f6c488
NaokiMinamikawa <naoki.minamikawa@omron.com>
2026-05-22 00:56:29 +0000

    fizzbuzz.jsを修正

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,15 +1,14 @@
-#!/usr/bin/env bash
+#!/usr/bin/env node
+const max = 20;
 
-const arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-arr.forEach((number) => {
+for (let number = 1; number <= max; number++) {
   if (number % 3 === 0 && number % 5 === 0) {
-    console.log("FizzBuzz"); 
-    } else if (number % 3 === 0) {
+    console.log("FizzBuzz");
+  } else if (number % 3 === 0) {
     console.log("Fizz");
-    } else if (number % 5 === 0) {
+  } else if (number % 5 === 0) {
     console.log("Buzz");
-    } else {
+  } else {
     console.log(number);
-    }
-});
-
+  }
+}
