c13f769911a0c3bd43e2e10db6390d66ff356cfc
NaokiMinamikawa <naoki.minamikawa@omron.com>
2026-07-15 08:14:35 +0000

    用途に応じた日付変数を用意

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -25,43 +25,22 @@ function cal(year, month) {
 }
 
 const args = process.argv.slice(2);
-let year, month;
+let c_year, c_month;
 
+// コマンドライン引数解析
 for (let i = 0; i < args.length; i++) {
   if (args[i] === "-y") {
-    year = parseInt(args[i + 1], 10);
+    c_year = parseInt(args[i + 1], 10);
   }
   if (args[i] === "-m") {
-    month = parseInt(args[i + 1], 10);
+    c_month = parseInt(args[i + 1], 10);
   }
 }
 
+// デフォルト値設定
 const today = new Date();
-year = year || today.getFullYear();
-month = month || today.getMonth() + 1;
+let d_year, d_month;
+d_year = c_year || today.getFullYear();
+d_month = c_month || today.getMonth() + 1;
 
-function cal(year, month) {
-  const firstDate = new Date(year, month - 1, 1);
-  const lastDate = new Date(year, month, 0);
-
-  console.log(`      ${month}月 ${year}`);
-  console.log("日 月 火 水 木 金 土");
-
-  const spaces = "   ".repeat(firstDate.getDay());
-  process.stdout.write(spaces);
-
-  for (
-    let current = new Date(year, month - 1, 1);
-    current.getMonth() === month - 1;
-    current.setDate(current.getDate() + 1)
-  ) {
-    const date = current.getDate();
-    process.stdout.write(`${date.toString().padStart(2, " ")} `);
-
-    if (current.getDay() === 6 || date === lastDate.getDate()) {
-      process.stdout.write("\n");
-    }
-  }
-}
-
-cal(year, month);
+cal(d_year, d_month);
