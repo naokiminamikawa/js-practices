dbd578989d82c5b2d7a0bf680e3ea75ebea9d177
NaokiMinamikawa <naoki.minamikawa@omron.com>
2026-07-15 08:12:26 +0000

    プログラムの実行順序変更

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,5 +1,29 @@
 #!/usr/bin/env node
 
+function cal(year, month) {
+  const firstDate = new Date(year, month - 1, 1);
+  const lastDate = new Date(year, month, 0);
+
+  console.log(`      ${month}月 ${year}`);
+  console.log("日 月 火 水 木 金 土");
+
+  const spaces = "   ".repeat(firstDate.getDay());
+  process.stdout.write(spaces);
+
+  for (
+    let current = new Date(year, month - 1, 1);
+    current.getMonth() === month - 1;
+    current.setDate(current.getDate() + 1)
+  ) {
+    const date = current.getDate();
+    process.stdout.write(`${date.toString().padStart(2, " ")} `);
+
+    if (current.getDay() === 6 || date === lastDate.getDate()) {
+      process.stdout.write("\n");
+    }
+  }
+}
+
 const args = process.argv.slice(2);
 let year, month;
 
